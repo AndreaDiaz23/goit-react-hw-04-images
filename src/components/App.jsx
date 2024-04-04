78af4542207aae8db95008ea0d680cc2371a3136
AndreaDiaz23 <andrea.grafico23@gmail.com>
2024-04-03 19:11:29 -0500

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@
   );
 }; */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery.jsx';
@@ -26,6 +26,86 @@ import Modal from './Modal/Modal.jsx';
 import Styles from './app.module.css';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+
+  useEffect(() => {
+    if (query) {
+      fetchImages();
+    }
+  }, [query]);
+
+  const fetchImages = () => {
+    const apiKey = '40998850-21238c0a5b68611eff0d55619';
+    const url = `https://pixabay.com/api/?q=${query}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`;
+
+    setLoading(true);
+
+    axios
+      .get(url)
+      .then((response) => {
+        setImages((prevImages) => [...prevImages, ...response.data.hits]);
+        setPage((prevPage) => prevPage + 1);
+      })
+      .catch((error) => console.error('Error fetching images:', error))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleSubmit = (query) => {
+    setQuery(query);
+    setImages([]);
+    setPage(1);
+  };
+
+  const handleLoadMore = async () => {
+    await fetchImages();
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  const handleImageClick = (largeImageURL) => {
+    setShowModal(true);
+    setLargeImageURL(largeImageURL);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setLargeImageURL('');
+  };
+
+  return (
+    <div className={Styles.App}>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery>
+        {images.map((image) => (
+          <ImageGalleryItem
+            key={image.id}
+            src={image.webformatURL}
+            alt={image.tags}
+            onClick={() => handleImageClick(image.largeImageURL)}
+          />
+        ))}
+      </ImageGallery>
+      <div className={Styles.Button_container}>
+        {loading && <Loader />}
+        {images.length > 0 && !loading && <Button onClick={handleLoadMore} />}
+        {showModal && <Modal onClose={handleCloseModal} largeImageURL={largeImageURL} />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+/* const App = () => {
   const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
@@ -99,3 +179,4 @@ const App = () => {
 };
 
 export default App;
+ */
